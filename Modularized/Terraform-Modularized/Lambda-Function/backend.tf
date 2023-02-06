40779b5056ef5002263926ff94f142ac11c5aee1
AmanPathak-dev <aman07pathak@gmail.com>
2023-02-06 15:59:41 +0530

    Backend Functionality Deleted

--- Modularized/Terraform-Modularized/Lambda-Function/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    
-    bucket         = "my-ews-baket"
-    region         = "us-east-1"
-    key            = "terraform.tfstate"
-    dynamodb_table = "Lock-Files"
-  }
-}
