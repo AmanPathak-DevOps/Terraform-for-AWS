59f3e55d90c6847470a54e6bbb47893e3b0184fb
AmanPathak-DevOps <aman07pathak@gmail.com>
2023-05-09 18:07:11 +0530

    Added Delete-Script for Terraform

---
+++ delete-script.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+find . -type d -name ".ter*" -exec rm -rf {} +;
+find . -type f -name ".ter*" -exec rm -rf {} +;
+find . -type d -name "ter*" -exec rm -rf {} +;
+find . -type f -name "ter*" -exec rm -rf {} +;
