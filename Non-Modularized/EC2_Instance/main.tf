faa9874e66d53954a30b6d66df2038fbf0246c36
AmanPathak-DevOps <aman07pathak@gmail.com>
2023-05-10 19:10:24 +0530

    Checkov Suggested Edit

--- Non-Modularized/EC2_Instance/main.tf
+++ Non-Modularized/EC2_Instance/main.tf
@@ -7,6 +7,16 @@ provider "aws" {
 resource "aws_instance" "ec2" {
   ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
+  ebs_optimized = true
+  monitoring = true
+  root_block_device {
+    encrypted     = true
+  }
+  metadata_options {
+        http_endpoint = "enabled"
+        http_tokens   = "required"
+  }
+  iam_instance_profile = "test"
   tags = {
     name = "Aman-EC2"
   }
