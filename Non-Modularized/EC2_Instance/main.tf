892e2b405cb5d8d865e1823e7276a5bd309f4bb5
AmanPathak-DevOps <aman07pathak@gmail.com>
2023-05-10 19:30:26 +0530

    Checkov Suggested Edit

--- Non-Modularized/EC2_Instance/main.tf
+++ Non-Modularized/EC2_Instance/main.tf
@@ -2,10 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
-
 resource "aws_instance" "ec2" {
-  ami           = "ami-04505e74c0741db8d"
+  ami           = "ami-007855ac798b5175e"
   instance_type = "t2.micro"
   ebs_optimized = true
   monitoring = true
