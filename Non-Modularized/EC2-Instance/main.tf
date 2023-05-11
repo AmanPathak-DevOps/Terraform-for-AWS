07e7d5f8791441b0bbbeeefe2d33e595d9986dbe
AmanPathak-DevOps <aman07pathak@gmail.com>
2023-05-11 14:03:59 +0530

    Updated main.tf file

--- Non-Modularized/EC2-Instance/main.tf
+++ Non-Modularized/EC2-Instance/main.tf
@@ -2,23 +2,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_iam_instance_profile" "example" {
-  name = "role-for-ec2"
-  role = aws_iam_role.iam-role.name
-}
 resource "aws_instance" "ec2" {
   ami           = "ami-007855ac798b5175e"
   instance_type = "t2.micro"
-  ebs_optimized = true
-  monitoring = true
-  root_block_device {
-    encrypted     = true
-  }
-  metadata_options {
-        http_endpoint = "enabled"
-        http_tokens   = "required"
-  }
-  iam_instance_profile = aws_iam_instance_profile.example.name
   tags = {
     name = "Aman-EC2"
   }
