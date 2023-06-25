d24358e6fab1cd6ce61370ae52ddc5c603b9fbd9
AmanPathak-DevOps <aman07pathak@gmail.com>
2023-06-25 17:39:45 +0530

    Added Github Action

--- Non-Modularized/ALB-EC2/backend.tf
+++ Non-Modularized/ALB-EC2/backend.tf
@@ -2,7 +2,15 @@ terraform {
   backend "s3" {
     bucket         = "my-ews-baket"
     region         = "us-east-1"
-    key            = "terraform.tfstate"
+    key            = "ALB-EC2/terraform.tfstate"
     dynamodb_table = "Lock-Files"
+    encrypt = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source = "hashicorp/aws"
+    }
   }
 }
--- Non-Modularized/ALB-EC2/provider.tf
+++ Non-Modularized/ALB-EC2/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region  = "us-east-1"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_access_key = var.AWS_SECRET_ACCESS_KEY  
+  # access_key = var.AWS_ACCESS_KEY_ID
+  # secret_access_key = var.AWS_SECRET_ACCESS_KEY  
 }
