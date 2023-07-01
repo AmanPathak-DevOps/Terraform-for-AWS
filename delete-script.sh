e3a853130ad383db15b37ce489370425f7a53e05
AmanPathak-DevOps <aman07pathak@gmail.com>
2023-07-01 22:53:28 +0530

    Updated AWS-Cost-Reporting Configuration

--- delete-script.sh
+++ delete-script.sh
@@ -2,5 +2,5 @@
 
 find . -type d -name ".ter*" -exec rm -rf {} +;
 find . -type f -name ".ter*" -exec rm -rf {} +;
-find . -type d -name "ter*" -exec rm -rf {} +;
-find . -type f -name "ter*" -exec rm -rf {} +;
+#find . -type d -name "ter*" -exec rm -rf {} +;
+#find . -type f -name "ter*" -exec rm -rf {} +;
