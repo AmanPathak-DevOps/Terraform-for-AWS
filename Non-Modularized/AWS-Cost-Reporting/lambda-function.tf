8a5689056fa2f94942b6ba38eb205bcbe012caf6
AmanPathak-DevOps <aman07pathak@gmail.com>
2023-07-01 23:18:03 +0530

    Update lambda code

--- Non-Modularized/AWS-Cost-Reporting/lambda-function.tf
+++ Non-Modularized/AWS-Cost-Reporting/lambda-function.tf
@@ -1,11 +1,11 @@
 resource "aws_lambda_function" "cost-reporting-lambda" {
-  filename         = "costusage.zip"
+  filename         = "python-code.zip"
   function_name    = "AWS-Cost-Reporting-Lambda"
   role             = aws_iam_role.iam-role.arn
   layers           = ["arn:aws:lambda:us-east-1:336392948345:layer:AWSSDKPandas-Python39:8", aws_lambda_layer_version.Datetime-layer.arn]
   handler          = "python-code.lambda_handler"
   runtime          = "python3.9"
-  source_code_hash = filebase64sha256("costusage.zip")
+  source_code_hash = filebase64sha256("python-code.zip")
 }
 
 resource "aws_lambda_permission" "eventbridge-permission" {
